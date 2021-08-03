d2353a03d5aebdbf87158f0ba1f2ef1c5ab126be
Yaison  Samboni <yaisonsamboni@Mac-mini-de-Yaison.local>
2021-08-02 22:26:49 -0500

    se corrije error encontrado en produccion donde la url provista por el api no funciona con https, se realiza correccion parcial en capa de api sin afectar capas superiores

--- src/app/datos/api/marvel.service.ts
+++ src/app/datos/api/marvel.service.ts
@@ -9,7 +9,7 @@ import { PersonajeAdapter } from '../adaptadores/personaje.adapter';
 import { ComicDto } from '../dto/comic.dto';
 import { ListaComicDto } from '../dto/listaComic.dto';
 import { ListaPersonajeDto } from '../dto/listaPersonaje.dto';
-import { API_KEY, BUSQUEDA_INICIA_CON, CANTIDAD_REGISTROS, PAGINA_SOLICITADA, SERVICIO_PERSONAJES } from './nombreServicios';
+import { API_KEY, BUSQUEDA_INICIA_CON, CANTIDAD_REGISTROS, COMPLEMENTO_COMICS_PERSONAJE, PAGINA_SOLICITADA, SERVICIO_COMICS, SERVICIO_COMICS_PERSONAJE, SERVICIO_PERSONAJES } from './nombreServicios';
 
 @Injectable({
   providedIn: 'root'
@@ -41,7 +41,9 @@ export class MarvelService {
   }
 
   getComicDesdeUrl(urlComic:String): Observable<ComicModel>{
-    let url = urlComic + `?${API_KEY}${environment.apiKey}`
+    let vectorUrl = urlComic.split('comics/')
+    let idComic : String = vectorUrl[1]
+    let url = environment.urlBase + SERVICIO_COMICS + idComic +`?${API_KEY}${environment.apiKey}`
     return new Observable(observador =>{
       this.http.get<ListaComicDto>(url).subscribe(respuesta =>{
         let comic: ComicModel = ComicAdapter.toModel(respuesta.data.results[0])
@@ -51,7 +53,10 @@ export class MarvelService {
   }
 
   getTodosComicsPersonajeDesdeUrl(urlComic:String): Observable<ComicModel[]>{
-    let url = urlComic + `?${API_KEY}${environment.apiKey}`
+    let vectorUrl = urlComic.split(SERVICIO_COMICS_PERSONAJE)
+    console.log(vectorUrl)
+    let idPersonaje : String = vectorUrl[1]
+    let url = environment.urlBase + SERVICIO_COMICS_PERSONAJE + idPersonaje + `?${API_KEY}${environment.apiKey}`
     return new Observable(observador =>{
       this.http.get<ListaComicDto>(url).subscribe(respuesta =>{
         let listaComics: ComicModel[] = ComicAdapter.listDtoToModel(respuesta.data.results)
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey : "079414ad5bfb29c5d2a99680e6257f43",
+  urlBase : "https://gateway.marvel.com:443/v1/public/"
 };
